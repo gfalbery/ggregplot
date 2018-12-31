a3005e5e9b01f39751372f938451a269f482f9e4
Greg Albery <gfalbery@gmail.com>
2018-12-31 18:37:44 -0500

    adding requires

--- R/Bar Graph.R
+++ R/Bar Graph.R
@@ -1,9 +1,11 @@
-############ Bar Graph 
+############ Bar Graph
 
-THEME <- theme(axis.text.x = element_text(size = 12, colour = "black"), 
-               axis.text.y = element_text(size = 12, colour = "black")) + 
-  theme(axis.title.x = element_text(vjust = -0.35), 
-        axis.title.y = element_text(vjust=1.2)) + 
+require(ggplot2)
+
+THEME <- theme(axis.text.x = element_text(size = 12, colour = "black"),
+               axis.text.y = element_text(size = 12, colour = "black")) +
+  theme(axis.title.x = element_text(vjust = -0.35),
+        axis.title.y = element_text(vjust=1.2)) +
   theme_bw()
 
 Prev<-function(y){ round((length(y[y>0])/length(y))*100)}
@@ -16,7 +18,7 @@ data_summary<-function(data, varname, groupnames){
       N=length(x[[col]]),
       se = sd(x[[col]], na.rm=TRUE)/sqrt(length(x[[col]])))
   }
-  
+
   data_sum<-plyr::ddply(data, groupnames, .fun=summary_func,
                   varname)
   data_sum <- plyr::rename(data_sum, c("mean" = varname))
@@ -31,11 +33,11 @@ BarGraph<-function(df,x,y,z=x,geom="Bar",text=FALSE,labels=NA){
   }else{
     df2$Prevalence<-na.omit(melt(tapply(df[,y],df[,x],Prev)))$value
   }
-  
+
   df2$Text<-df2[,text]
-  
+
   PositionT<-(range(c(df2[,y]-df2$se,df2[,y]+df2$se),na.rm=T)[2]-range(c(df2[,y]-df2$se,df2[,y]+df2$se),na.rm=T)[1])/15
-  
+
   if(geom=="Bar"){
     if(text%in%c("Prevalence","N")){
       ggplot(df2,aes(df2[,x],df2[,y],fill=as.factor(df2[,z])))+
--- R/MCMC Effect Size Comparison.R
+++ R/MCMC Effect Size Comparison.R
@@ -1,114 +1,116 @@
 # Liverpool era effect size comparison ####
 
 MCMCFxComp<-function(model, vars, numeric = T){
-  
+
+  require(MCMCglmm)
+
   FactorCompList<-list()
-  
+
   Solutions<-model$Sol[,1:dim(model$X)[2]]
-  
+
   if(!is.null(vars)){
     for(x in 1:length(vars)){
-      
+
       Factors <- vars[x]
       Columns<-Solutions[,substr(colnames(Solutions),1,nchar(Factors))==Factors]
       if(dim(Columns)[2]>1){
         Columns <- cbind(0, Columns)
         df1<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) mean(a - b)[1]))
-        
+
         df1l<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) HPDinterval(as.mcmc(a - b))[1]))
-        
+
         df1u<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) HPDinterval(as.mcmc(a - b))[2]))
-        
-        df1p<-apply(Columns,2,function(b) 
+
+        df1p<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) (dim(Columns)[1]-max(table(a>b)))/(dim(Columns)[1]/2)))
-        
+
         #df1[,1]<-summary(model)$solutions[rownames(summary(model)$solutions)%in%colnames(Columns),"post.mean"]
-        
+
         dim(df1)<-rep(ncol(Columns),2)
         dimnames(df1) <- dimnames(df1l) <- dimnames(df1u) <- dimnames(df1p) <-list(substr(colnames(Columns),nchar(Factors)+1,100),substr(colnames(Columns),nchar(Factors)+1,100))
-        colnames(df1)[1] <- colnames(df1l)[1] <- colnames(df1u)[1] <- colnames(df1p)[1] <- "Intercept" 
-        rownames(df1)[1] <- rownames(df1l)[1] <- rownames(df1u)[1] <- rownames(df1p)[1] <- "Intercept" 
+        colnames(df1)[1] <- colnames(df1l)[1] <- colnames(df1u)[1] <- colnames(df1p)[1] <- "Intercept"
+        rownames(df1)[1] <- rownames(df1l)[1] <- rownames(df1u)[1] <- rownames(df1p)[1] <- "Intercept"
         FactorCompList[[x]] <- list(Mean=df1, Lower=df1l, Upper=df1u, pMCMC=df1p)
         names(FactorCompList)[x]<-Factors
-        
+
       }else{
         Column<-colnames(Solutions)[substr(colnames(Solutions),1,nchar(Factors))==Factors]
         df1<-summary(model)$solutions[Column,1]
         df1l<-summary(model)$solutions[Column,2]
         df1u<-summary(model)$solutions[Column,3]
         df1p<-summary(model)$solutions[Column,"pMCMC"]
-        
+
         FactorCompList[[x]]<-list(Mean=df1, Lower=df1l, Upper=df1u, pMCMC=df1p)
         names(FactorCompList)[x]<-Factors
       }
     }
-    
+
     if(numeric == F){
       FactorCompList[[x]] <- paste0(round(df1,3), " (", round(df1l,3), ", ", round(df1u,3), ") ",
-                                    cut(df1p,breaks=c(-0.1,0.001,0.01,0.05,1),labels=c("***","**","*",""))) 
-      
+                                    cut(df1p,breaks=c(-0.1,0.001,0.01,0.05,1),labels=c("***","**","*","")))
+
       if(length(FactorCompList[[x]])>0){
         dim(FactorCompList[[x]])<-rep(ncol(Columns),2)
         dimnames(FactorCompList[[x]])<-list(substr(colnames(Columns),nchar(Factors)+1,100),substr(colnames(Columns),nchar(Factors)+1,100))
       }
     }
-    
+
   } else {
     Factors <- unique(unlist(strsplit(substr(unlist(summary(model)$fixed.formula)[3],1,1000),split=" ")))
     Factors<-Factors[-which(Factors=="+")]
-    
+
     Solutions<-model$Sol[,1:dim(model$X)[2]]
     for(x in 1:length(Factors)){
       Columns<-Solutions[,substr(colnames(Solutions),1,nchar(Factors))==Factors]
       if(dim(Columns)[2]>1){
         Columns <- cbind(0, Columns)
-        
+
         df1<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) mean(a - b)[1]))
-        
+
         df1l<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) HPDinterval(as.mcmc(a - b))[1]))
-        
+
         df1u<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) HPDinterval(as.mcmc(a - b))[2]))
-        
-        df1p<-apply(Columns,2,function(b) 
+
+        df1p<-apply(Columns,2,function(b)
           apply(Columns,2,function(a) (dim(Columns)[1]-max(table(a>b)))/(dim(Columns)[1]/2)))
-        
-        
+
+
         dim(df1)<-rep(ncol(Columns),2)
         dimnames(df1)<-list(substr(colnames(Columns),nchar(Factors)+1,100),substr(colnames(Columns),nchar(Factors)+1,100))
-        colnames(df1)[1] <- colnames(df1l)[1] <- colnames(df1u)[1] <- colnames(df1p)[1] <- "Intercept" 
-        rownames(df1)[1] <- rownames(df1l)[1] <- rownames(df1u)[1] <- rownames(df1p)[1] <- "Intercept" 
+        colnames(df1)[1] <- colnames(df1l)[1] <- colnames(df1u)[1] <- colnames(df1p)[1] <- "Intercept"
+        rownames(df1)[1] <- rownames(df1l)[1] <- rownames(df1u)[1] <- rownames(df1p)[1] <- "Intercept"
         FactorCompList[[x]]<- list(Mean = df1, Lower = df1l, Upper = df1u, pMCMC = df1p)
         names(FactorCompList)[x]<-Factors
-        
+
       }else{
         Column<-colnames(Solutions)[substr(colnames(Solutions),1,nchar(Factors))==Factors]
-        df1<-summary(model)$solutions[Column,1]        
+        df1<-summary(model)$solutions[Column,1]
         df1l<-summary(model)$solutions[Column,2]
         df1u<-summary(model)$solutions[Column,3]
         df1p<-summary(model)$solutions[Column,"pMCMC"]
-        
+
         FactorCompList[[x]]<-list(Mean = df1, Lower = df1l, Upper = df1u, pMCMC = df1p)
         names(FactorCompList)[x]<-Factors
       }
     }
-    
+
     if(numeric == F){
       FactorCompList[[x]] <- paste0(round(df1,3), " (", round(df1l,3), ", ", round(df1u,3), ") ",
-                                    cut(df1p,breaks=c(-0.1,0.001,0.01,0.05,1),labels=c("***","**","*",""))) 
-      
+                                    cut(df1p,breaks=c(-0.1,0.001,0.01,0.05,1),labels=c("***","**","*","")))
+
       if(length(FactorCompList[[x]])>0){
         dim(FactorCompList[[x]])<-rep(ncol(Columns),2)
         dimnames(FactorCompList[[x]])<-list(substr(colnames(Columns),nchar(Factors)+1,100),substr(colnames(Columns),nchar(Factors)+1,100))
       }
     }
   }
-  
+
   FactorCompList
-  
+
 }
