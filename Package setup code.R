8e8e0a6d007230f9e5afdcabc6484833f241d2d4
Greg Albery <gfalbery@gmail.com>
2018-12-31 18:18:29 -0500

    full upload

---
+++ Package setup code.R
@@ -0,0 +1,10 @@
+
+# Establishing ggregplot functions
+
+funcroot <- "D:/Scripy/Graveyard of Obscure Functions"
+source(paste0(funcroot, "/Model Effects Plot.R"))
+source(paste0(funcroot, "/ggplot INLA Field.R"))
+source(paste0(funcroot, "/INLA DIC Plot.R"))
+source(paste0(funcroot, "/INLA Range Plot.R"))
+
+library(devtools)
