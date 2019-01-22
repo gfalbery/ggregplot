89f78a2ced95347522c0621c1abc650961640cd4
Greg Albery <gfalbery@gmail.com>
2019-01-22 12:09:43 -0500

    Shapes changed in competitive models

--- R/INLA DIC Plot.R
+++ R/INLA DIC Plot.R
@@ -9,7 +9,9 @@ INLADICFig <- function(ModelList, ModelNames = NULL){
 
   df$Competitive <- with(df, ifelse(DIC<min(DIC + 2), "Y", "N"))
 
-  ggplot(df, aes(as.numeric(Model), DIC)) + geom_point() + geom_line() + THEME +
+  ggplot(df, aes(as.numeric(Model), DIC)) +
+    geom_point(aes(shape = Competitive)) + scale_shape_manual(values = c(1,2)) +
+    geom_line() + THEME +
     scale_x_continuous(breaks = as.numeric(df$Model),labels = ModelNames) + geom_point(data = df[df$Competitive == "Y",])
 
 }
