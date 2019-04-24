31f26bfd340c11f4df8351276905daa154322859
Greg Albery <gfalbery@gmail.com>
2019-04-24 16:16:31 +0100

    Sorting Order argument

--- R/Bar Graph.R
+++ R/Bar Graph.R
@@ -16,28 +16,29 @@ BarGraph <- function(df, x, y, z = x,
               N = n()) %>%
     mutate(se = sd/(N^0.5)) %>%
     as.data.frame() %>%
-    slice(order(Mean, decreasing = T))
+    slice(order(Mean, decreasing = T)) %>% na.omit()
+
+  if(Order) Errordf <- Errordf %>% slice(order(Mean)) %>% mutate(X = factor(X, levels = unique(X)), Colour = factor(Colour, levels = unique(X)))
 
   SPlot <- df %>%
-    ggplot(aes(as.factor(X), Y, fill = as.factor(Colour))) +
+    ggplot(aes(X, Y, fill = Colour)) +
     labs(x = x, y = y) +
     geom_col(data = Errordf,
-             aes(y = Mean, group = as.factor(Colour)),
+             aes(y = Mean, group = Colour),
              colour = "black",
              position = position_dodge(0.9)) +
 
     geom_errorbar(data = Errordf, inherit.aes = F,
-                  aes(x = as.factor(X),
+                  aes(x = X,
                       ymin = Mean - se,
                       ymax = Mean + se,
-                      group = as.factor(Colour)),
+                      group = Colour),
                   width = 0.1,
                   colour = "black",
                   position = position_dodge(0.9))
 
   if(z != y) SPlot <- SPlot + labs(colour = z)
 
-  if(Order) SPlot <- SPlot + scale_x_discrete(limits = rev(unique(Errordf$X)))
   if(Just) SPlot <- SPlot + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   if(Text == "N"){
