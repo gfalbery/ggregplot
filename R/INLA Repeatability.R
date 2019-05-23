c13e5f635f14c99680df19be1cf633bbd86661ee
Greg Albery <gfalbery@gmail.com>
2019-05-23 11:55:10 +0100

    choo choo changing repeatability calculations

--- R/INLA Repeatability.R
+++ R/INLA Repeatability.R
@@ -1,4 +1,5 @@
 
+
 # INLA ICC function ####
 
 library(tidyverse)
@@ -27,15 +28,28 @@ INLARep <- function(Model, Family = "gaussian"){
 
   names(SigmaList) <- NameList
 
+  if(any(names(Model$marginals.hyperpar) %>%substr(1,5)=="Range")){
+
+    Var <- names(Model$marginals.hyperpar)[which(names(Model$marginals.hyperpar) %>%substr(1,5)=="Range")]
+    Expl <- Var %>% str_split(" ") %>% last %>% last
+
+    i1 = inla.spde.result(Model, Expl, spde)
+    i2 = i1$marginals.tau[[1]] %>% inla.tmarginal(function(a) 1/a, .) %>% inla.mmarginal()
+
+    SigmaList$SPDE <- i2
+
+  }
+
   if(Family == "binomial"){
-    lapply(SigmaList, function(a) (a^2)/c(sum(sapply(SigmaList, function(b) b^2))+pi^(2/3))) %>% return
+    lapply(SigmaList, function(a) (a)/c(sum(sapply(SigmaList, function(b) b))+pi^(2/3))) %>% return
   } else{
 
-    if(Family == "gaussian") lapply(SigmaList, function(a) a^2/(sum(sapply(SigmaList, function(b) b^2)))) %>% return
+    if(Family == "gaussian") lapply(SigmaList, function(a) a/(sum(unlist(SigmaList)))) %>% return
+    #if(Family == "gaussian") lapply(SigmaList, function(a) a) %>% return
   }
 }
 
-INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F, DICOrder = F, Family = "gaussian"){
+INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F, DICOrder = F, Family = "gaussian", Residual = T){
 
   OutputList <- sapply(ModelList, function(a) INLARep(a, Family = Family)) %>% bind_rows %>% data.frame
   OutputList$Model <- as.numeric(rownames(OutputList)) %>% as.factor
@@ -47,7 +61,11 @@ INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F, DICOrder = F, Fa
   if(DICOrder) OutputList <- OutputList[rev(order(sapply(ModelList, MDIC))),]; OutputList$Model <- factor(OutputList$Model, levels = unique(OutputList$Model))
 
   OutputLong <- OutputList %>% gather(Var, Variance, -"Model") %>%
-    mutate(Var = factor(Var, levels = unique(Var)))
+    mutate(Var = factor(Var, levels = unique(Var))) %>% na.omit()
+
+  if(!Residual){
+    OutputLong <- OutputLong %>% filter(!Var == "Residual")
+  }
 
   if(Just){ Angle = 45; Hjust = 1 }else{ Angle = 0; Hjust = 0.5}
 
