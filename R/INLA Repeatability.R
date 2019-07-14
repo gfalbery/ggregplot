59244975177cba5e3790792cebb8b4119718c002
Greg Albery <gfalbery@gmail.com>
2019-07-14 12:46:10 +0100

    Trying to allow for SE estimation

--- R/INLA Repeatability.R
+++ R/INLA Repeatability.R
@@ -6,18 +6,17 @@ library(tidyverse)
 
 INLARep <- function(Model, Family = "gaussian"){
 
-  MySqrt <- function(x) {1 / sqrt(x)}
-
-  SigmaList <- list()
+  SigmaList <- CIList <- list()
 
   Parameters <- which(names(Model$marginals.hyperpar) %>%substr(1,9)%in%c("Precision","precision"))
   HyperPars <- Model$marginals.hyperpar[Parameters]
 
   for(x in 1:(length(HyperPars))){
     tau <- HyperPars[[x]]
-    sigma <- inla.emarginal(MySqrt, tau)
     sigma <- inla.emarginal(function(x) 1/x, tau)
     SigmaList[[x]] <- sigma
+    ci <- tau %>% inla.tmarginal(function(a) 1/a, .) %>% inla.hpdmarginal(p = 0.95)
+    CIList[[x]] <- ci
   }
 
   NameList <- sapply(names(HyperPars), function(a) last(strsplit(a, " ")[[1]]))
@@ -38,30 +37,90 @@ INLARep <- function(Model, Family = "gaussian"){
 
     SigmaList$SPDE <- i2
 
+    ci <- i1$marginals.tau[[1]] %>% inla.tmarginal(function(a) 1/a, .) %>% inla.hpdmarginal(p = 0.95)
+    CIList$SPDE <- ci
+
   }
 
   if(Family == "binomial"){
-    lapply(SigmaList, function(a) (a)/c(sum(sapply(SigmaList, function(b) b))+pi^(2/3))) %>% return
-  } else{
 
-    if(Family == "gaussian") lapply(SigmaList, function(a) a/(sum(unlist(SigmaList)))) %>% return
-    #if(Family == "gaussian") lapply(SigmaList, function(a) a) %>% return
+    ReturnList <- sapply(SigmaList, function(a) (a)/c(sum(sapply(SigmaList, function(b) b))+pi^(2/3)))
+
+    LowerList <- sapply(map(CIList, 1), function(a) (a)/c(sum(sapply(SigmaList, function(b) b))+pi^(2/3)))
+    UpperList <- sapply(map(CIList, 2), function(a) (a)/c(sum(sapply(SigmaList, function(b) b))+pi^(2/3)))
+
   }
+
+  if(Family == "gaussian"){
+
+    ReturnList <- sapply(SigmaList, function(a) a/(sum(unlist(SigmaList))))
+
+    LowerList <- sapply(map(CIList, 1), function(a) (a)/c(sum(unlist(SigmaList))))
+    UpperList <- sapply(map(CIList, 2), function(a) (a)/c(sum(unlist(SigmaList))))
+
+  }
+
+  ReturnDF <- data.frame(
+    Mean = ReturnList,
+    Lower = LowerList,
+    Upper = UpperList
+  )
+
+  return(ReturnDF)
 }
 
-INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F, DICOrder = F, Family = "gaussian", Residual = T){
+INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F,
+                        DICOrder = F, CutOff = 0,
+                        Family = "gaussian", Residual = T, CI = F,
+                        Position = "stack", Plot = T){
 
-  OutputList <- sapply(ModelList, function(a) INLARep(a, Family = Family)) %>% bind_rows %>% data.frame
-  OutputList$Model <- as.numeric(rownames(OutputList)) %>% as.factor
+  require(tidyverse); require(INLA); require(ggregplot)
 
-  if(!is.null(ModelNames)){
-    levels(OutputList$Model) <- ModelNames
+  OutputList <- lapply(ModelList, function(a) INLARep(a, Family = Family))
+
+  for(i in 1:length(OutputList)){
+    OutputList[[i]] <- OutputList[[i]] %>% mutate(
+      Model = names(OutputList)[i],
+      Var = rownames(OutputList[[i]])
+    )
   }
 
-  if(DICOrder) OutputList <- OutputList[rev(order(sapply(ModelList, MDIC))),]; OutputList$Model <- factor(OutputList$Model, levels = unique(OutputList$Model))
+  OutputList <- OutputList %>% bind_rows() %>%
+    mutate(Var = factor(Var, levels = unique(Var)),
+           Model = factor(Model, levels = unique(Model)))
 
-  OutputLong <- OutputList %>% gather(Var, Variance, -"Model") %>%
-    mutate(Var = factor(Var, levels = unique(Var))) %>% na.omit()
+  if(CI){
+
+    for(i in unique(OutputList$Model)){
+
+      Subdf <- OutputList %>% filter(Model == i) %>% slice(order(Var))
+      N = Subdf %>% nrow
+      Sums = c(1, 1-cumsum(Subdf$Mean))
+      Sums = Sums[-length(Sums)]
+      OutputList[OutputList$Model == i,c("Lower", "Upper")] <-
+        OutputList %>% filter(Model == i) %>%
+        mutate(Lower = Lower + Sums - Mean,
+               Upper = Upper + Sums - Mean) %>% select(Lower, Upper)
+
+    }
+
+  }
+
+  if(DICOrder){
+
+    OutputList <- OutputList %>%
+      mutate(Model = factor(OutputList$Model,
+                            levels = levels(OutputList$Model)[(order(sapply(ModelList, MDIC), decreasing = T))]))
+
+    OutputList$DIC <- sapply(ModelList, MDIC)[as.character(OutputList$Model)]
+    OutputList$Competitive <- ifelse(OutputList$DIC<(min(OutputList$DIC)+CutOff), "*", " ")
+
+  }
+
+  OutputLong <- OutputList %>% rename(Variance = Mean)
+  OutputLong[,c("Lower","Upper")] <- OutputLong %>%
+    mutate(Lower = ifelse(Upper-Lower<0.01, NA, Lower),
+           Upper = ifelse(Upper-Lower<0.01, NA, Upper)) %>% select(Lower, Upper)
 
   if(!Residual){
     OutputLong <- OutputLong %>% filter(!Var == "Residual")
@@ -69,10 +128,72 @@ INLARepPlot <- function(ModelList, ModelNames = NULL, Just = F, DICOrder = F, Fa
 
   if(Just){ Angle = 45; Hjust = 1 }else{ Angle = 0; Hjust = 0.5}
 
-  ggplot(OutputLong, aes(factor(Model), Variance, fill = Var)) +
-    geom_col(position = "stack") +
+  RepPlot <- ggplot(OutputLong, aes(factor(Model), Variance, fill = Var)) +
+    geom_col(position = Position) +
     labs(x = "Model") +
-    theme(axis.text.x = element_text(angle = Angle, hjust = Hjust)) +
-    lims(y = c(0,1))
+    theme(axis.text.x = element_text(angle = Angle, hjust = Hjust))
+
+  if(DICOrder&CutOff>0){
+
+    RepPlot <- RepPlot  + geom_text(aes(y = 0.95, label = Competitive))
+
+  }
+
+  if(CI){
+
+    RepPlot <- RepPlot +
+      geom_errorbar(aes(ymin = Lower, ymax = Upper), width = 0.2) +
+      lims(y = c(0,max(OutputLong$Upper)))
+
+  } else{
+    RepPlot <- RepPlot +
+      lims(y = c(0,1))
+
+  }
+
+  if(Plot){
+    return(RepPlot)
+  }else{
+    return(OutputLong)
+  }
 
 }
+
+INLARepPlot(CentralityList[[1]], DICOrder = F, Just = T, CI = T)
+
+x = 8
+
+ModelList = CentralityList[[1]][x]
+
+OutputList <- lapply(ModelList, function(a) INLARep(a, Family = Family))
+
+for(i in 1:length(OutputList)){
+  OutputList[[i]] <- OutputList[[i]] %>% mutate(
+    Model = names(OutputList)[i],
+    Var = rownames(OutputList[[i]])
+  )
+}
+
+OutputList <- OutputList %>% bind_rows() %>%
+  mutate(Var = factor(Var, levels = unique(Var)),
+         Model = factor(Model, levels = unique(Model)))
+
+i = names(CentralityList[[1]])[x]
+Subdf <- OutputList# %>% filter(Model == i)
+N = Subdf %>% nrow
+Sums = c(1, 1-cumsum(Subdf$Mean))
+Sums = Sums[-length(Sums)]
+OutputList[OutputList$Model == i,c("Lower", "Upper")] <-
+  OutputList %>% filter(Model == i) %>%
+  mutate(Lower = Lower + Sums - Mean,
+         Upper = Upper + Sums - Mean) %>% select(Lower, Upper)
+
+OutputLong <- OutputList %>% rename(Variance = Mean)
+
+ggplot(OutputLong, aes(factor(Model), Variance, fill = Var)) +
+  geom_col(position = Position) +
+  labs(x = "Model") +
+  theme(axis.text.x = element_text(angle = Angle, hjust = Hjust)) +
+  #lims(y = c(0,1.5)) +
+  geom_errorbar(aes(ymin = Lower, ymax = Upper), width = 0.3)
+
