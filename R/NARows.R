887a53bc3b5934cea05b670f08c3dd46a78966dd
CGMossa <cgmossa@gmail.com>
2019-09-24 18:48:36 +0200

    Added documentation

--- R/NARows.R
+++ R/NARows.R
@@ -1,5 +1,14 @@
-# Removing NA Rows ####
-
+#'  Removing NA Rows
+#'
+#' @param df
+#' @param vars
+#'
+#' @return
+#'
+#' @note This function is effectively [stats::na.omit()], thus this is not
+#' exported
+#'
+#' @examples
 NARows <-function(df, vars){
   apply(as.data.frame(df[,vars]), 1, function(a){
     any(is.na(a)|a=="Inf"|a=="-Inf")
