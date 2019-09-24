0c280093303ca0cd04f9f60e27c5213848b2f8c2
CGMossa <cgmossa@gmail.com>
2019-09-24 18:47:56 +0200

    Added documentation

--- R/Nice_Scale_Function.R
+++ R/Nice_Scale_Function.R
@@ -1,5 +1,18 @@
-# non-problematic scale function ####
-
+#' Non-problematic scale function
+#'
+#' Normalises a vector `x` such that mean is zero and variance is 1.
+#'
+#' Missing values are ignored by default.
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @seealso For a version that works on `data.frame` see [scale()].
+#'
+#' @examples
 scale_this <- function(x){
   (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
 }
+
