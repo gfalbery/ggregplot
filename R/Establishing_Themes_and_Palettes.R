0b6d9c6c68aafd86c262d18affff505b79eaa084
CGMossa <cgmossa@gmail.com>
2019-09-24 19:24:40 +0200

    I don't know what to do about this..

--- R/Establishing_Themes_and_Palettes.R
+++ R/Establishing_Themes_and_Palettes.R
@@ -1,22 +1,24 @@
 # Establishing Themes and Palettes ####
 
-library(ggplot2);
-library(RColorBrewer)
+# requireNamespace("ggplot2", quietly = TRUE)
+# requireNamespace("RColorBrewer", quietly = TRUE)
 
-ParasitePalettes<-c("PuRd","PuBu","BuGn","Purples","Oranges")
-ParasiteColours<-c("#DD1c77","#2B8CBE","#2CA25F",brewer.pal(5,"Purples")[4],brewer.pal(5,"Oranges")[4])
+ParasitePalettes <- c("PuRd","PuBu","BuGn","Purples","Oranges")
+ParasiteColours  <- c("#DD1c77","#2B8CBE","#2CA25F",
+                   RColorBrewer::brewer.pal(5,"Purples")[4],
+                   RColorBrewer::brewer.pal(5,"Oranges")[4])
 
 AlberPalettes <- c("YlGnBu","Reds","BuPu", "PiYG")
 AlberColours <- sapply(AlberPalettes, function(a) RColorBrewer::brewer.pal(5, a)[4])
 AlberColours[length(AlberColours)+1:2] <- RColorBrewer::brewer.pal(11, AlberPalettes[[4]])[c(2,10)]
 
-AlberTheme <- theme_bw() +
-  theme(axis.title.x = element_text(vjust = -0.35,
+AlberTheme <- ggplot2::theme_bw() +
+  ggplot2::theme(axis.title.x = ggplot2::element_text(vjust = -0.35,
                                     size = 12,
                                     colour = "black"),
-        axis.title.y = element_text(vjust = 1.2,
+        axis.title.y = ggplot2::element_text(vjust = 1.2,
                                     size = 12,
                                     colour = "black"),
-        strip.background = element_rect(fill = "white", colour = "dark grey"))
+        strip.background = ggplot2::element_rect(fill = "white", colour = "dark grey"))
 
-theme_set(AlberTheme)
+ggplot2::theme_set(AlberTheme)
