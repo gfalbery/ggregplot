f6c843eac04a99d80bc16fa925bdc611185c9f77
Greg Albery <gfalbery@gmail.com>
2020-03-01 14:49:00 -0500

    GregTime Works

--- R/GregTime.R
+++ R/GregTime.R
@@ -1,29 +1,27 @@
 
 # GregTime ####
 
-# when I run this code, specifying DecTime and Decs, it pops out the right answer
-DecTime = 0.053
-Decs = 6
+GregTime <- function(DecTime){
 
-DecTime <- 0.5
+  HourTime <- as.numeric(DecTime)*24
 
-#gregtime <- function(DecTime, Decs=6) {
+  HourCharacter <- floor(HourTime)
 
-HourTime <- as.numeric(DecTime)*24
-HourCharacter <- floor(HourTime)
-MinuteCharacter <- (HourTime - HourCharacter)*60
-HourCharacter <- HourCharacter %>% as.character %>%
-  str_pad("0", side = "left", width = 2)
-H <- HourCharacter
-M <- MinuteCharacter  %>% floor %>% str_pad("0", side = "left", width = 2)
-S <- MinuteCharacter  %>% subtract(as.numeric(M)) %>% multiply_by(6000)
-if(Decs == 6){
-  paste(H, M, S %>% substr(1,2),
-        sep = ":")
+  MinuteCharacter <- (HourTime - HourCharacter)*60
+
+  HourCharacter <- HourCharacter %>% as.character %>%
+    str_pad("0", side = "left", width = 2)
+
+  H <- HourCharacter %>% str_pad("0", side = "left", width = 2)
+
+  M <- MinuteCharacter %>% floor %>% str_pad("0", side = "left", width = 2)
+
+  S <- MinuteCharacter %>% subtract(as.numeric(M)) %>%
+    multiply_by(6000) %>% substr(1,2) %>% str_pad("0", side = "left", width = 2)
+
+  paste(H, M, S, sep = ":")
+
 }
-#}
-
-
 
 
 
