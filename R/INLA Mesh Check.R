5fcf01c50032f055e751050a11e77668e22d49e2
Greb <gfalbery@gmail.com>
2020-04-18 02:06:38 -0400

    _Massive_ mesh overhaul yas

--- R/INLA Mesh Check.R
+++ R/INLA Mesh Check.R
@@ -5,12 +5,22 @@ GetMesh <- function(Mesh){
 
   Mesh$loc %>% as.data.frame() %>% rename(X = V1, Y = V2) %>% mutate(Vertex = 1:n()) -> Vertices
 
-  Mesh$graph$vv %>% as.matrix %>% reshape2::melt() %>% rename(From = Var1, To = Var2) %>%
-    filter(value == 1) %>% mutate(Group = 1:n()) %>% dplyr::select(-value) -> Edges
+  Mesh$graph$vv %>% as.matrix %>%
+    reshape2::melt() %>%
+    rename(From = Var1, To = Var2) %>%
+    filter(value == 1) %>%
+    mutate(Group = 1:n()) %>%
+    dplyr::select(-value) ->
 
-  Edges %>% gather("ToFrom", "Vertex", -Group) -> LongEdges
+    Edges
 
-  LongEdges %>% left_join(Vertices, by = c("Vertex")) -> LongEdges
+  Edges %>% gather("ToFrom", "Vertex", -Group) ->
+
+    LongEdges
+
+  LongEdges %>% left_join(Vertices, by = c("Vertex")) ->
+
+    LongEdges
 
   return(
 
@@ -26,7 +36,7 @@ GetMesh <- function(Mesh){
 
 }
 
-ggMesh <- function(Mesh, Include = c("Edges")){
+ggMesh <- function(Mesh, Include = c("Edges"), Alpha = 1){
 
   GotMesh <- GetMesh(Mesh)
 
@@ -37,14 +47,15 @@ ggMesh <- function(Mesh, Include = c("Edges")){
   if("Edges" %in% Include){
 
     MeshPlot <- MeshPlot +
-      geom_path(data = GotMesh$Edges, aes(group = Group))
+      geom_path(data = GotMesh$Edges, aes(group = Group),
+                alpha = Alpha)
 
   }
 
   if("Vertices" %in% Include){
 
     MeshPlot <- MeshPlot +
-      geom_point()
+      geom_point(alpha = Alpha)
 
   }
 
@@ -53,3 +64,72 @@ ggMesh <- function(Mesh, Include = c("Edges")){
 }
 
 ggMesh(Mesh)
+
+
+
+Vertices[,c("X", "Y")] %>% SpatialPointsDataFrame(data = ., coords = .) %>% st_as_sf()
+
+Vertices[,c("X", "Y")] %>% chull -> Hull
+
+Vertices[Hull,] -> Hull
+
+Hull %>% dplyr::select(-c(X, Y, V3)) -> HullVertexNames
+
+HullVertexNames %>% SpatialPointsDataFrame(data = ., coords = Hull[,c("X", "Y")]) %>% plot
+
+Hull[,c("X", "Y")] %>% Polygon %>% list %>%
+  Polygons(ID = '0') %>%
+  list %>% SpatialPolygons -> Outline
+
+plot(Outline)
+
+Range <- 0.2
+
+Outline %>% st_as_sf %>% st_buffer(Range) %>% plot
+
+Hull[,c("X", "Y")] %>% slice(n():1) %>%
+  Polygon(hole = T) %>% list %>%
+  Polygons(ID = '0') %>%
+  list %>% SpatialPolygons -> Outline
+
+plot(Outline)
+
+Range <- 0.2
+
+Outline %>% st_as_sf %>% st_buffer(Range) %>% plot
+
+
+CheckMeshBoundaries <- function(TestDf, Mesh, Range, HullColour = "black", ...){
+
+  TestDF %>% chull -> Hull
+
+  TestDF[Hull,] -> Hull
+
+  Hull[,c("X", "Y")] %>%
+    Polygon() %>% list %>%
+    Polygons(ID = '0') %>%
+    list %>% SpatialPolygons ->
+
+    Outline
+
+  Outline %>% st_as_sf %>% st_buffer(Range) -> Buffered
+  Buffered$geometry[[1]][[1]] %>% as.data.frame() %>% rename(X = V1, Y = V2) -> BufferedDF
+
+  MeshPlot <- ggMesh(Mesh, ...)
+
+  MeshPlot +
+    geom_path(data = BufferedDF, inherit.aes = F, aes(X, Y), colour = HullColour) +
+    geom_point(data = TestDF, inherit.aes = F, aes(X, Y))
+
+  # Mesh bit ####
+
+  GotMesh <- GetMesh(Mesh, ...)
+
+  GotMesh$Vertices[,c("X", "Y")]
+
+    geom_polygon() +
+    geom_path(data = BufferedDF, inherit.aes = F, aes(X, Y), colour = HullColour) +
+    geom_point(data = TestDF, inherit.aes = F, aes(X, Y))
+
+}
+
