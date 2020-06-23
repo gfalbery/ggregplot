72973735aec64eb7f88177d98e42e1f463f012eb
Greb <gfalbery@gmail.com>
2020-06-22 21:31:30 -0400

    Fixing P values

--- R/INLA P Value.R
+++ R/INLA P Value.R
@@ -1,22 +1,38 @@
 
-INLAPValue <- function(Model, Vars = NULL, Decimals = 6){
-  
-  if(is.null(Vars)){
-    
-    Vars <- names(Model$marginals.fixed)
-    
-  }
-  
-  Vars %>% map(~Model$marginals.fixed[[.x]] %>% 
-                 
-                 inla.rmarginal(marginal = ., 10^Decimals) %>% 
-                 
-                 PCalc %>% return
-                 
-                 ) -> Return
+INLAPValue <-
 
-  names(Return) <- Vars
-  
-  return(Return)
-  
-}
+  function(Model, Vars = NULL, Decimals = 6, Method = "MCMC"){
+
+    if(is.null(Vars)){
+
+      Vars <- names(Model$marginals.fixed)
+
+    }
+
+    if(Method == "MCMC"){
+
+      Vars %>% map(~Model$marginals.fixed[[.x]] %>%
+
+                     inla.rmarginal(marginal = ., 10^Decimals) %>%
+
+                     PCalc %>% return
+
+      ) -> Return
+
+    }else if(Method == "INLA"){
+
+      Vars %>% map(~Model$marginals.fixed[[.x]] %>%
+
+                     (1 - inla.pmarginal(0, marginal = .))*2 %>%
+
+                     return
+
+      ) -> Return
+
+    }
+
+    names(Return) <- Vars
+
+    return(Return)
+
+  }
--- R/MCMC Variance Component P Calculation.R
+++ R/MCMC Variance Component P Calculation.R
@@ -1,4 +1,20 @@
-PCalc<-function(Vector){
-  table(Vector>0)
-  min(table(Vector>0))/(length(Vector)/2)
+PCalc <- function(Vector, Minimum = "Minimum"){
+
+  table(Vector > 0)
+
+  Value <- min(table(Vector > 0))/
+    (length(Vector)/2)
+
+  if(Value == 2){
+
+    if(Minimum == "Minimum"){
+
+      Value <- 1/length(Vector)
+
+    }else if(Minimum == "Zero"){
+
+      Value <- 0
+
+    }
+  }
 }
