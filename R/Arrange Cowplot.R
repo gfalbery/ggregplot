7dde45ef8336cd6983e8eafaa5609b2f110449f4
Greb <gfalbery@gmail.com>
2020-11-04 16:37:15 +0000

    Oops it's patchwork not cowplot

--- R/Arrange Cowplot.R
+++ R/Arrange Cowplot.R
@@ -1,6 +1,20 @@
 
 # Arrange Cowplot
 
+ArrangePatchwork <- function(PlotList){
+
+  Plot <- PlotList[[1]]
+
+  for(i in 2:length(PlotList)){
+
+    Plot <- Plot + PlotList[[i]]
+
+  }
+
+  return(Plot)
+
+}
+
 ArrangeCowplot <- function(PlotList){
 
   Plot <- PlotList[[1]]
