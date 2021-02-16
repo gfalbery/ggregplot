dc99e9c6387eb43685d89c4621ca38799163f450
Greb <gfalbery@gmail.com>
2021-02-16 16:31:22 +0000

    Adding remove diagonal from propmatrix

--- R/ProportionalMatrix.R
+++ R/ProportionalMatrix.R
@@ -1,13 +1,15 @@
-ProportionalMatrix <- function(Matrix, Observations){
-  
+ProportionalMatrix <- function(Matrix, Observations, RemoveDiagonal = T){
+
+  if(RemoveDiagonal) diag(Matrix) <- 0
+
   N <- Matrix %>% nrow
-  
+
   A <- rep(Observations, each = N)
   B <- rep(Observations, N)
-  
+
   AMatrix <- matrix(A, ncol = N)
   BMatrix <- matrix(B, ncol = N)
-  
+
   Matrix/(AMatrix + BMatrix - Matrix)
-  
+
 }
