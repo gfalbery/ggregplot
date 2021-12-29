4abda12813be681661b6cb7e373454e22c565574
gfalbery <gfalbery@gmail.com>
2021-12-29 15:11:41 +0000

    Adding the deer version

--- R/MakePredictDF.R
+++ R/MakePredictDF.R
@@ -1,29 +1,64 @@
 
 # MakePredictDF ####
 
-MakePredictDF <- function(DF, Length.Out = 100){
-  
+MakePredictDF <- function(DF,
+                          Length.Out = 100,
+                          HoldNumeric = NULL,
+                          HoldFactor = NULL){
+
   PredList <- list()
-  
-  for(i in 1:ncol(DF)){
-    
-    if(class(DF[,i]) %in% c("numeric", "integer")){
-      
-      PredList[[i]] <- c(seq(from = min(DF[,i]),
-                             to = max(DF[,i]),
+
+  NumericDF <- DF %>%
+    dplyr::select_if(is.numeric)
+
+  if(ncol(NumericDF) > 0){
+
+    for(i in 1:ncol(NumericDF)){
+
+      PredList[[i]] <- c(seq(from = min(NumericDF[,i]),
+                             to = max(NumericDF[,i]),
                              length.out = Length.Out),
-                         mean(DF[,i]))
-      
-    }else{
-      
-      PredList[[i]] <- unique(DF[,i])
-        
+                         mean(NumericDF[,i]))
+
     }
-    
+
+    names(PredList) <- colnames(NumericDF)
+
+    if(length(intersect(HoldNumeric, names(PredList))) > 0){
+
+      intersect(HoldNumeric, names(PredList)) %>%
+        map(function(a){
+
+          PredList[[a]] <<- last(PredList[[a]])
+
+        })
+    }
   }
-  
-  names(PredList) <- colnames(DF)
-  
+
+  FactorDF <- DF %>% dplyr::select_if(~is.character(.x)|is.factor(.x))
+
+  if(ncol(FactorDF) > 0){
+
+    for(i in 1:ncol(FactorDF)){
+
+      PredList[[length(PredList) + 1]] <- c(unique(FactorDF[,i]))
+
+    }
+
+    names(PredList)[(ncol(NumericDF) + 1):length(PredList)] <- c(colnames(FactorDF))
+
+    if(length(intersect(names(HoldFactor), names(PredList))) > 0){
+
+      intersect(names(HoldFactor), names(PredList)) %>%
+
+        map(function(a){
+
+          PredList[[a]] <<- HoldFactor[[a]]
+
+        })
+    }
+  }
+
   return(PredList)
-  
+
 }
