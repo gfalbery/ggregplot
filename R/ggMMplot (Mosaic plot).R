3e3672d6e474ba9a083dc584d4b7d74ef15a2165
Your Name <gfalbery@gmail.com>
2023-09-04 13:33:28 -0400

    Fixing ggMMplot

--- R/ggMMplot (Mosaic plot).R
+++ R/ggMMplot (Mosaic plot).R
@@ -1,47 +1,50 @@
 ############ ggMMplot ##############
 
-ggMMplot <- function(df, var1, var2, Alpha = 1, Just = F){
+ggMMplot  <-  function(DF, Var1, Var2, Alpha = 1, Just = F){
+
   require(ggplot2)
 
   if(Just){
 
-    Angle <- 45
-    HJust <- 0
+    Angle  <-  45
+    HJust  <-  0
 
   }else{
 
-    Angle <- 0
-    HJust <- 0.5
+    Angle  <-  0
+    HJust  <-  0.5
 
   }
 
-  df2<-na.omit(df[,c(var1,var2)])
+  DF2 <- na.omit(DF[,c(Var1,Var2)])
 
-  df2[,var1]<-as.factor(df2[,var1])
-  df2[,var2]<-as.factor(df2[,var2])
+  DF2[,Var1] <- as.factor(DF2[,Var1])
+  DF2[,Var2] <- as.factor(DF2[,Var2])
 
-  levVar1 <- length(levels(df2[,var1]))
-  levVar2 <- length(levels(df2[,var2]))
+  levVar1  <-  length(levels(DF2[,Var1]))
+  levVar2  <-  length(levels(DF2[,Var2]))
 
-  jointTable <- prop.table(table(df2[,var1], df2[,var2]))
-  plotData <- as.data.frame(jointTable)
+  jointTable  <-  prop.table(table(DF2[,Var1], DF2[,Var2]))
+  plotData  <-  as.data.frame(jointTable)
 
-  plotData$marginVar1 <- prop.table(table(df2[,var1]))
+  plotData$marginVar1  <-  prop.table(table(DF2[,Var1]))
 
-  plotData$var2Height <- plotData$Freq / plotData$marginVar1
+  plotData$Var2Height  <-  plotData$Freq / plotData$marginVar1
 
-  plotData$var1Center <- c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
+  plotData$Var1Center  <-  c(0, cumsum(plotData$marginVar1)[1:levVar1 -1]) +
     plotData$marginVar1 / 2
 
-  plotData$N<-c(table(df2[,var1],df2[,var2]))
+  plotData$N <- c(table(DF2[,Var1],DF2[,Var2]))
 
-  ggplot(plotData, aes(var1Center, var2Height)) +
-    geom_bar(stat = "identity", aes(width = marginVar1,
-                                    fill = Var2),
-             col = "Black", alpha = Alpha) +
+  ggplot(plotData, aes(Var1Center, Var2Height)) +
+    geom_col(#stat = "identity",
+      width = plotData$marginVar1,
+      aes(#width = marginVar1,
+        fill = Var2),
+      col = "Black", alpha = Alpha) +
     geom_text(aes(label = as.character(Var1),
                   hjust = HJust, angle = Angle,
-                  x = var1Center, y = 1.05)) +
-    labs(x = NULL,y = NULL, fill = var2)
+                  x = Var1Center, y = 1.05)) +
+    labs(x = NULL,y = NULL, fill = Var2)
 
 }
