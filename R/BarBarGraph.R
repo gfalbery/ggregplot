e95fa1932019071afb0196dd1cc847911f333ee9
Your Name <gfalbery@gmail.com>
2024-03-19 21:33:49 -0400

    Fixing barbargraph

--- R/BarBarGraph.R
+++ R/BarBarGraph.R
@@ -1,32 +1,84 @@
 ############# BarBarGraph
 
-data_summary <- function(data, varname, groupnames){
-  require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm = TRUE),
-      sd = sd(x[[col]], na.rm = TRUE),
-      N = length(x[[col]]),
-      se = sd(x[[col]], na.rm = TRUE)/sqrt(length(x[[col]])))
-  }
+# data_summary <- function(data, varname, groupnames){
+#   # require(plyr)
+#   summary_func <- function(x, col){
+#     c(mean = mean(x[[col]], na.rm = TRUE),
+#       sd = sd(x[[col]], na.rm = TRUE),
+#       N = length(x[[col]]),
+#       se = sd(x[[col]], na.rm = TRUE)/sqrt(length(x[[col]])))
+#   }
+#
+#   data_sum<-ddply(data, groupnames, .fun = summary_func,
+#                   varname)
+#   data_sum <- rename(data_sum, c("mean" = varname))
+#   return(data_sum)
+# }
 
-  data_sum<-ddply(data, groupnames, .fun = summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
-  return(data_sum)
-}
-
-BarBarGraph <- function(data,group,x,y){
+BarBarGraph <- function(Data, group, x, y, AddPoints = F){
 
   require(magrittr)
 
-  data %<>% as.data.frame
+  BarFun <- mean
 
-  df2 <- na.omit(data.frame(cbind(data_summary(data,varname = x,groupnames = group),
-                                  data_summary(data,varname = y,groupnames = group)[,c(y,"se")])))
+  Data %<>% as.data.frame
 
-  ggplot(na.omit(df2),aes(df2[,x],df2[,y],colour = df2[,group]))+
-    geom_errorbar(aes(ymin = df2[,y]-se.1,ymax = df2[,y]+se.1),width = diff(range(df2[,x]))/20,size = 1)+
-    geom_errorbarh(aes(xmin = df2[,x]-se,xmax = df2[,x]+se),height = diff(range(df2[,y]))/20,size = 1)+
-    labs(x = x,y = y,colour = group)
+  Data <- Data[,c(x,y, group)] %>% na.omit()
+
+  Data$X <- Data[,x]
+  Data$Y <- Data[,y]
+  Data$Colour <- Data[,group]
+
+  if(!is.factor(Data[,group])){
+
+    Data$Colour %<>% factor(levels = sort(unique(Data[,group])))
+
+  }
+
+  Errordf <- Data %>% group_by(Colour) %>%
+    summarise(MeanY = BarFun(Y, na.rm = T),
+              sdY = sd(Y, na.rm = T),
+              MeanX = BarFun(X, na.rm = T),
+              sdX = sd(X, na.rm = T),
+              N = n()) %>%
+    mutate(seY = sdY/(N^0.5)) %>%
+    mutate(YLower = MeanY - seY,
+           YUpper = MeanY + seY) %>%
+    mutate(seX = sdX/(N^0.5)) %>%
+    mutate(XLower = MeanX - seX,
+           XUpper = MeanX + seX) %>%
+    as.data.frame() %>%
+    arrange(desc(MeanX), desc(MeanY)) %>% na.omit()
+
+  SPlot <- Data %>%
+    ggplot(aes(X, Y, colour = Colour)) +
+    labs(x = x, y = y, colour = group)
+
+  if(AddPoints){
+
+    SPlot <- SPlot + geom_point()
+
+  }
+
+  SPlot <- SPlot +
+
+    geom_errorbar(data = Errordf, inherit.aes = F,
+                  aes(x = MeanX,
+                      ymin = YLower,
+                      ymax = YUpper,
+                      group = Colour),
+                  # width = 0.1,
+                  colour = "black") +
+
+    geom_errorbarh(data = Errordf, inherit.aes = F,
+                   aes(y = MeanY,
+                       xmin = XLower,
+                       xmax = XUpper,
+                       group = Colour),
+                   # width = 0.1,
+                   colour = "black")
+
+  SPlot %>% return
 
 }
+
